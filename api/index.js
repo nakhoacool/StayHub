d68e5afd307eef416d7c978c6813bc6bb23c330e
Khoa Nguyen <77623180+nakhoacool@users.noreply.github.com>
2023-11-22 10:44:54 +0700

    Add input type 'number' to index.css and implement image upload functionality

--- api/index.js
+++ api/index.js
@@ -5,13 +5,14 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const imageDownloader = require('image-downloader')
 const app = express()
 
 const UserModel = require('./models/User')
 
 app.use(express.json())
 app.use(cookieParser())
-
+app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use(
   cors({
     credentials: true,
@@ -80,6 +81,16 @@ app.get('/profile', (req, res) => {
 //logout
 app.post('/logout', (req, res) => {
   res.clearCookie('token').json({ message: 'Logged out' })
+})
+
+app.post('/upload-by-link', async (req, res) => {
+  const { link } = req.body
+  const newName = 'photo' + Date.now() + '.jpg'
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + '/uploads/' + newName,
+  })
+  res.json(newName)
 })
 
 app.listen(3000, () => {
