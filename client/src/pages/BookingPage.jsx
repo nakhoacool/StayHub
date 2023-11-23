55758d6a9805b829d620f9d5c06e24ec29c7dc22
Khoa Nguyen <77623180+nakhoacool@users.noreply.github.com>
2023-11-23 17:22:50 +0700

    Add booking details and loading state to BookingPage component

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,6 +1,18 @@
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function BookingPage() {
   const { id } = useParams()
-  return <div>single booking: {id}</div>
+  const [booking, setBooking] = useState(null)
+  useEffect(() => {
+    axios.get('/get-bookings').then((response) => {
+      const foundBooking = response.data.find(({ _id }) => _id === id)
+      if (foundBooking) setBooking(foundBooking)
+    })
+  }, [id])
+
+  if (!booking) return <div>loading...</div>
+
+  return <div></div>
 }
