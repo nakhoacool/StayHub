5ac3c18705aba118263fc475395869b1ec7e0f00
Khoa Nguyen <77623180+nakhoacool@users.noreply.github.com>
2023-11-23 17:36:01 +0700

    Refactor booking page and bookings page components

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -2,6 +2,9 @@ import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+import AddressLink from '../components/AddressLink'
+import PlaceGallery from '../components/PlaceGallery'
+
 export default function BookingPage() {
   const { id } = useParams()
   const [booking, setBooking] = useState(null)
@@ -14,5 +17,14 @@ export default function BookingPage() {
 
   if (!booking) return <div>loading...</div>
 
-  return <div></div>
+  return <div className='my-8'>
+    <h1 className='text-3xl'>
+      {booking.place.title}
+    </h1>
+    <AddressLink place={booking.place} className={'my-2 block'} />
+    <div className="bg-gray-200 p-4 mb-4 rounded-2xl">
+      <h2 className='text-xl'>Your booking information</h2>
+    </div>
+    <PlaceGallery place={booking.place} />
+  </div>
 }
